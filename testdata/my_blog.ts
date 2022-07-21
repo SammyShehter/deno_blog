d3d7637fbdab5b90bbbad9f31723ac464c568d6c
SammyShehter <sammyshehter@gmail.com>
2022-07-21 18:28:08 +0300

    personal use

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -1,14 +1,15 @@
 import blog from "../blog.tsx";
 
 blog({
-  author: "Dino",
-  title: "My Blog",
-  description: "The blog description.",
-  avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  author: "Sammy Shehter",
+  title: "@sammyshehter",
+  description: "Happy to see you ^^",
+  avatar: "https://avatars.githubusercontent.com/u/51539466?s=400&u=d75fa91b0939920239d7fc65be539e967669c0c2&v=4",
   avatarClass: "rounded-full",
+  port:6000,
   links: [
-    { title: "bot@deno.com", url: "mailto:bot@deno.com" },
-    { title: "GitHub", url: "https://github.com/denobot" },
-    { title: "Twitter", url: "https://twitter.com/denobot" },
+    { title: "Email", url: "mailto:sammyshehter@gmail.com" },
+    { title: "GitHub", url: "https://github.com/SammyShehter" },
+    { title: "Twitter", url: "https://twitter.com/sammyshehter" },
   ],
 });
---
+++ testdata/posts/hello_world.md
@@ -0,0 +1,19 @@
+---
+title: Hello World
+publish_date: 2022-07-21
+abstract: This is the first post.
+---
+
+Wrote my (unfortunate) [blog](https://github.com/SammyShehter/blogue_v3) until I found [this](https://deno.land/x/blog) solution.
+I think I'll explore this repository and try to contribute to in it.
+Contacted one of the participants, I hope my contribution will be useful.
+In general, Deno pleasantly surprised.
+
+<img src="media/hello_world/1.jpg"/>
+
+Other news -
+I bought a standing table. kewl.
+
+
+What I like the most is that it uses .md markup
+I can definitely adapt it to my needs
