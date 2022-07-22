f52b3f79ab38a307b7f3f54c06f8584be862f75b
sammyshehter <samir_shehter@yahoo.com>
2022-07-22 12:47:03 +0300

    blog settings changes

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -1,12 +1,13 @@
 import blog from "../blog.tsx";
 
 blog({
-  author: "Sammy Shehter",
-  title: "@sammyshehter",
-  description: "Happy to see you ^^",
+  author: "Sammy",
+  title: "Sammy Shehter",
+  description: "@sammyshehter",
   avatar: "https://avatars.githubusercontent.com/u/51539466?s=400&u=d75fa91b0939920239d7fc65be539e967669c0c2&v=4",
   avatarClass: "rounded-full",
   port:6000,
+  theme:"dark",
   links: [
     { title: "Email", url: "mailto:sammyshehter@gmail.com" },
     { title: "GitHub", url: "https://github.com/SammyShehter" },
